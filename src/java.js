b6b8a8ce35635184182013a774947cc3e95ece3a
8Ball0 <zaymoody1110@gmail.com>
2025-05-30 17:07:21 -0400

    Added a function to search API

--- src/java.js
+++ src/java.js
@@ -1,8 +1,18 @@
+
+
+function cityLookup(city){
+   let apiKey = "4tbcf6d8oa89ad8e3f7bdb3c1fa0deb0";
+   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+   axios.get(apiURL).then(updateWeather)
+
+}
+
 function changeCity(event){
     event.preventDefault();
     let cityInput = document.querySelector("#search-form-id");
     let title = document.querySelector("#city-title");
     title.innerHTML = cityInput.value;
+    cityLookup(cityInput.value)
 }
 
 let searchForm = document.querySelector("#search-form");
