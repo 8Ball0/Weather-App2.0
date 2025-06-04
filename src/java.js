a2e535ef56b4fb44aea47ee0bb7fe51e64300ced
8Ball0 <zaymoody1110@gmail.com>
2025-06-04 17:35:11 -0400

    Added function to search city data
    Had to restart and rewrite functions to change city title and update HTML. Also was able to search api and find correct data for city searched and log temperature

--- src/java.js
+++ src/java.js
@@ -1,19 +1,24 @@
 
 
-function cityLookup(city){
-   let apiKey = "4tbcf6d8oa89ad8e3f7bdb3c1fa0deb0";
-   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
-   axios.get(apiURL).then(updateWeather)
+function updateWeather(response){
+    console.log(response.data.temperature.current);
+}
 
+function lookupCityInfo(city){
+    apiKey = "4tbcf6d8oa89ad8e3f7bdb3c1fa0deb0"
+    apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeather)
 }
 
 function changeCity(event){
     event.preventDefault();
-    let cityInput = document.querySelector("#search-form-id");
-    let title = document.querySelector("#city-title");
-    title.innerHTML = cityInput.value;
-    cityLookup(cityInput.value)
+    let titleChange = document.querySelector("#search-form-id");
+    let titleOriginal = document.querySelector("#city-title");
+    titleOriginal.innerHTML = titleChange.value;
+    lookupCityInfo(titleChange.value);
 }
 
-let searchForm = document.querySelector("#search-form");
+ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeCity);
+
+
