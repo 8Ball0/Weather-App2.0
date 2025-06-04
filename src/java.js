090abb3ce4256d2b4e54800ecc67f5bb7f99473f
8Ball0 <zaymoody1110@gmail.com>
2025-06-04 18:22:43 -0400

    Update wind and humidity data

--- src/java.js
+++ src/java.js
@@ -4,9 +4,17 @@ function updateWeather(response){
     let tempOriginal = document.querySelector("#current-weather-number-id")
     let temperature = Math.round(response.data.temperature.current);
     let titleOriginal = document.querySelector("#city-title");
+    let conditions = document.querySelector("#weather-conditions");
+    let currentConditions = response.data.condition.description;
+    let humidity = document.querySelector("#humidity");
+    let winds = document.querySelector("#winds")
+
     tempOriginal.innerHTML = temperature;
     titleOriginal.innerHTML = response.data.city
-    console.log(temperature); 
+    conditions.innerHTML = currentConditions;
+    humidity.innerHTML = response.data.temperature.humidity;
+    winds.innerHTML = response.data.wind.speed
+  
 }
 
 function lookupCityInfo(city){
